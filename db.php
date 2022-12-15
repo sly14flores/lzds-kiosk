82fcd5066dab442dd60dac76b91e4d6014523eb8
Sly Flores <sly@christian.com.ph>
2022-12-15 21:10:51 +0800

    ...

--- db.php
+++ db.php
@@ -26,7 +26,7 @@ class pdo_db {
 		$db_name = "monitoring";
 		$dsn = "mysql:host=$server;dbname=$db_name;charset=utf8";
 		$username = "root";
-		$password = "sly";
+		$password = "";
 
 		$this->db = new PDO($dsn, $username, $password, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		$this->table = $table;
